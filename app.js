46f464112f282d82c8adac4d3d517a4f89f5f04d
Hidan12 <marcarfe12@gmail.com>
2022-12-21 02:27:00 -0500

    test7

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const app = express();
 
 app.use(express.static("public"));
 app.listen(process.env.PORT || 3000, function() {console.log("Servidor corriendo");});
-app.get("/", (_, res)=>{res.sendFile(__dirname + "/views/index.html")})
+app.get("/", (_, res)=>{res.sendFile(__dirname + "/views/index.html")});
 
